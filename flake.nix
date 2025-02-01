238422db78c2ef852366ba3c34eb8ab8371b2915
cosbit <cosmechavez314@gmail.com>
2025-01-31 17:49:38 -0800

    actual initial release, working audio, wifi and clock

--- flake.nix
+++ flake.nix
@@ -8,6 +8,16 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in {
+    
+    packages.${system}.default = ags.lib.bundle {
+      inherit pkgs; 
+
+      src = ./.;
+      name = "cag-shell";
+      entry = "app.ts";
+      gtk4 = false;
+
+    };
     devShells.${system}.default = pkgs.mkShell {
       buildInputs = [
         # includes astal3 astal4 astal-io by default
--- src/audio/Audio.tsx
+++ src/audio/Audio.tsx
@@ -1,6 +1,28 @@
 import { Variable } from "astal";
 import { exec } from "astal/process";
+import { Gtk } from "astal/gtk3";
 import { Icon, Overlay, Slider } from "astal/gtk3/widget";
+import { bind, Subscribable } from "astal/binding";
+
+/**
+ * Initialize Volume
+ * 
+ * Get from pactl sink
+ * It returns left and right, we have to get either of them.
+ * Then convert to int -> this is the current volume as int
+ * We initialize volume to be set to that int.
+ * 
+*/
+const execstr = exec(`pactl get-sink-volume @DEFAULT_SINK@`);
+const match = execstr.match(/(\d+)%/);
+const volumeint = match ? parseInt(match[1], 10) : 0;
+
+const volume = Variable(volumeint);
+
+volume.subscribe((value: number) => {
+  let rounded = Math.round(value);
+  exec(`pactl set-sink-volume @DEFAULT_SINK@ ${value}%`);
+});
 
 export default function Audio() {
   return (
@@ -12,7 +34,28 @@ export default function Audio() {
           <label label={"firefox"} />
         </box>
       </box>
-      <box className={"audio-tile-middle"}></box>
+      <box className={"audio-tile-middle"}>
+        <Overlay
+          className={"audio-overlay"}
+          child={
+            <Slider
+              hexpand
+              draw_value={false}
+              min={0}
+              max={100}
+              value={volumeint}
+              onDragged={(self) => volume.set(self.value)}
+            />
+          }
+          overlay={
+            <Icon
+              className={"audio-icon"}
+              halign={Gtk.Align.START}
+              icon="saturn"
+            />
+          }
+        />
+      </box>
       <box className={"audio-tile-lower"}></box>
     </box>
   );
